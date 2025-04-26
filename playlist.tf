9ef2d048acf7fa26650075d8622f6cd00cee949b
mayur <mayurdalimbkar26@gmail.com>
2025-04-26 20:01:45 +0530

    files added

---
+++ playlist.tf
@@ -0,0 +1,34 @@
+resource "spotify_playlist" "playlist" {
+  name        = "My playlist"
+  description = "My playlist is so awesome"
+  public      = false
+
+  tracks = [
+    data.spotify_track.overkill.id,
+    data.spotify_track.blackwater.id,
+    data.spotify_track.snowblind.id,
+  ]
+}
+
+data "spotify_track" "overkill" {
+  url = "https://open.spotify.com/track/4XdaaDFE881SlIaz31pTAG"
+}
+data "spotify_track" "blackwater" {
+  url = "https://open.spotify.com/track/4lE6N1E0L8CssgKEUCgdbA"
+}
+data "spotify_track" "snowblind" {
+  url = "https://open.spotify.com/track/7FCG2wIYG1XvGRUMACC2cD"
+}
+
+resource "spotify_playlist" "ariana_grande" {
+  name        = "My Ariana Grande Playlist"
+
+  tracks = flatten([
+    spotify_search_track.ariana_grande.tracks[*].id,
+  ])
+}
+
+data "spotify_search_track" "ariana_grande" {
+  artist = "Ariana Grande"
+  limit = 10
+}
